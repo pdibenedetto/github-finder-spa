90a9b8e8aad6185b6873e9aaff23d2dbf6c992f8
Paul <pdibenedetto@users.noreply.github.com>
2022-03-15 16:43:57 -0400

    Removing .env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
+  // add daisyUI plugin
   plugins: [require('daisyui')],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  },
 }
